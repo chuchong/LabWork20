6bc92c47431ea7aca4e54af625518a96cb32ea3e
chuchong <33688310+chuchong@users.noreply.github.com>
2020-06-07 02:14:46 +0800

    implementation of triangle and box overlap

--- geometry.py
+++ geometry.py
@@ -8,7 +8,7 @@ import numpy as np
 to calculate whether the vector that equals v0 v1 edge (x, y, z) and X/Y/Z axis dot
 can separate the triangle and box
 
-the dot matrix of (x, y, z) is 
+the cross matrix of (x, y, z) is 
     0   -z  y
     z   0   -x
     -y  x   0
@@ -41,7 +41,7 @@ if False in (project_V_in 1 or project_V_in 2 or project_V_in 3)
 return false
 '''
 
-def dotMatrix(e):
+def crossMatrix(e):
     return np.array([[0, -e[2], e[1]],
                      [e[2], 0, -e[0]],
                      [-e[1], e[0], 0]])
@@ -59,19 +59,39 @@ def triBoxOverlap(box_center, halfsize_box, triverts):
     '''
     relateive_triverts = triverts - box_center
 
+    # 9 cross axis
     for i in range(3):
         j = (i + 1) % 3
 
         e = triverts[j] - triverts[i]
-        dot_matrix = dotMatrix(e)
-        radius = np.fabs(np.dot(dot_matrix, np.transpose(halfsize_box)).reshape(-1,1))
-        projection = np.transpose(dot_matrix)
+        cross_matrix = crossMatrix(e)
+        radius = np.fabs(np.dot(cross_matrix, np.transpose(halfsize_box)).reshape(-1,1))
+        projection = np.transpose(cross_matrix)
         project_v = np.dot(projection, np.transpose(relateive_triverts))
         in_bool = np.bitwise_or(np.all( - radius > project_v ,axis=1), np.all( project_v > radius, axis=1))
         if in_bool.any():
             return False
 
+    # x, y, z, axis
+    in_bool = np.bitwise_or(np.all(-halfsize_box > relateive_triverts, axis = 1),
+                            np.all(halfsize_box < relateive_triverts, axis = 1))
 
+    if in_bool.any():
+        return False
 
+    '''
+    finally calculate the normal of the triangle
+    we can also use the cross product
+    e1 = v2 - v1
+    e2 = v3 - v1
+    e1 cross e2 = v2 X v3 + v3 X v1 + v1 X v2
+    '''
+
+    cross_projection = np.cross(triverts[1] - triverts[0], triverts[2] - triverts[0])
+    project_v = np.dot(cross_projection, np.transpose(relateive_triverts[0]))
+    radius = np.fabs(np.dot(cross_projection, np.transpose(halfsize_box)))
+
+    if project_v > radius or project_v < -radius:
+        return False
     return True
 
